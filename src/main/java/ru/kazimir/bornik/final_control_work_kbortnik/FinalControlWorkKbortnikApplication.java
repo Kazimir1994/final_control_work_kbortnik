862066a9a6ed66b49b9806c31e238eaf640089ab
Kazimir1994 <kazimir_bortnik@mail.ru>
2019-06-05 18:15:56 +0300

    project initialization

---
+++ src/main/java/ru/kazimir/bornik/final_control_work_kbortnik/FinalControlWorkKbortnikApplication.java
@@ -0,0 +1,13 @@
+package ru.kazimir.bornik.final_control_work_kbortnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalControlWorkKbortnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalControlWorkKbortnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kazimir/bornik/final_control_work_kbortnik/FinalControlWorkKbortnikApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.kazimir.bornik.final_control_work_kbortnik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalControlWorkKbortnikApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
